a3e59bcf0f692caa332bc9b273c1eabbdda03cd7
Berkay <berkaycirak_11@hotmail.com>
2022-06-04 20:26:19 +0300

    Second Commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,79 @@
+import { useNavigate, useLocation } from 'react-router-dom';
+//Since we want to use our svg files as a component, we import those like below.
+import { ReactComponent as OfferIcon } from '../assets/svg/localOfferIcon.svg';
+import { ReactComponent as ExploreIcon } from '../assets/svg/exploreIcon.svg';
+import { ReactComponent as PersonOutlineIcon } from '../assets/svg/personOutlineIcon.svg';
+
+function Navbar() {
+  //useNavigate will return a function that helps us to navigate what we want.
+  const navigate = useNavigate();
+  //useLocation will return a location object, when we navigate our page, that location object changes according to page.
+  console.log('hello');
+  const location = useLocation();
+
+  // This function will check the route and location whether true or not.
+  const pathMatchRoute = (route) => {
+    if (route === location.pathname) {
+      return true;
+    }
+  };
+
+  return (
+    <footer className='navbar'>
+      <nav className='navbarNav'>
+        <ul className='navbarListItems'>
+          <li className='navbarListItem' onClick={() => navigate('/')}>
+            <ExploreIcon
+              fill={pathMatchRoute('/') ? '#2c2c2c' : '#8f8f8f'}
+              width='36px'
+              height='36px'
+            />
+            <p
+              className={
+                pathMatchRoute('/')
+                  ? 'navbarListItemNameActive'
+                  : 'navbarListItemName'
+              }
+            >
+              Explore
+            </p>
+          </li>
+          <li className='navbarListItem' onClick={() => navigate('/offers')}>
+            <OfferIcon
+              fill={pathMatchRoute('/offers') ? '#2c2c2c' : '#8f8f8f'}
+              width='36px'
+              height='36px'
+            />
+            <p
+              className={
+                pathMatchRoute('/offers')
+                  ? 'navbarListItemNameActive'
+                  : 'navbarListItemName'
+              }
+            >
+              Offer
+            </p>
+          </li>
+          <li className='navbarListItem' onClick={() => navigate('/profile')}>
+            <PersonOutlineIcon
+              fill={pathMatchRoute('/profile') ? '#2c2c2c' : '#8f8f8f'}
+              width='36px'
+              height='36px'
+            />
+            <p
+              className={
+                pathMatchRoute('/profile')
+                  ? 'navbarListItemNameActive'
+                  : 'navbarListItemName'
+              }
+            >
+              Profile
+            </p>
+          </li>
+        </ul>
+      </nav>
+    </footer>
+  );
+}
+
+export default Navbar;
