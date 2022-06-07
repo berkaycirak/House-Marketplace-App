2c341e0afde1fff312da06bcc21c89ba511363f0
Berkay <berkaycirak_11@hotmail.com>
2022-06-07 22:07:09 +0300

    One bug is fixed

--- src/hooks/useAuthStatus.js
+++ src/hooks/useAuthStatus.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
+// We will check whether the currentUser is matching with userRef(the actual user of the items.)
 export function useAuthStatus() {
   const [loggedIn, setLoggedIn] = useState(false);
   const [checkingStatus, setCheckingStatus] = useState(true);
